040cca961ca8f30846ab6b7dbf79e2881ccae493
Kevin Le <kle9293@gmail.com>
2025-07-23 01:09:03 -0700

    Tutorial Template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ChatMessage from './ChatMessage';
+// TODO: Fill In Clad Import
 
 export default function App() {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
+	const [latestResponse, setLatestResponse] = useState(null); // store response globally
+
+	// 1. TODO: Initialize Clad Client
+  const cladClient = new CladClient({
+    apiKey: process.env.REACT_APP_CLAD_API_KEY,
+    threshold: 1,
+  });
+
+	// 2. TODO: Initialize User ID
+  const userId = cladClient.getOrCreateUserId();
+
+  useEffect(() => {
+    const run = async () => {
+      // 3. TODO: Call function to inject ad into inline text response
+      const response = 
+			setLatestResponse(response);
+    };
+
+    run();
+  }, []);
 
   const handleSend = () => {
     if (!input.trim()) return;
@@ -17,6 +38,11 @@ export default function App() {
     if (e.key === 'Enter') handleSend();
   };
 
+	// 5. TODO: Make a new object to have a map to the response from #4
+	const adMessage = {
+		prompted: latestResponse
+	}
+
   return (
     <div className="chat-container">
       <div className="messages">
@@ -24,6 +50,8 @@ export default function App() {
           <ChatMessage key={idx} sender={msg.sender} text={msg.text} />
         ))}
       </div>
+			{/* 6 TODO: Conditionally render the AdCard if the response is injected */}
+		
       <div className="input-box">
         <input
           value={input}
