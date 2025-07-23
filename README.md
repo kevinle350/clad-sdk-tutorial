e49b997330f9b639467d7e1e810cec3fa3f1ea29
Kevin Le <kle9293@gmail.com>
2025-07-23 01:05:25 -0700

    simple example

--- README.md
+++ README.md
@@ -19,3 +19,9 @@ Open http://localhost:3000 to view it in your browser.
 
 ### 4. Test with Clad-SDK
 https://www.npmjs.com/package/@clad-ai/react
+
+- npm i @clad-ai/react
+- In App.js Follow the Instantiating CladClient and getProcessedInput step in the docs
+    - Use the prompt "Im looking for a new coffee shop in San Jose"
+    - Change the threshold to 1
+- Check the webapp in the browser and check the console logs and if SDK working successfully you should see a response
--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ChatMessage from './ChatMessage';
+// 1. Clad Imports
+import { AdCard, CladClient } from '@clad-ai/react';
 
 export default function App() {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
+	const [latestResponse, setLatestResponse] = useState(null); // ✅ store response globally
+
+	// 2. Initialize Clad Client
+  const cladClient = new CladClient({
+    apiKey: process.env.REACT_APP_CLAD_API_KEY,
+    threshold: 1,
+  });
+
+	// 3. Initialize User ID
+  const userId = cladClient.getOrCreateUserId();
+
+  useEffect(() => {
+    const run = async () => {
+      // 4. Call function to inject ad into inline text response
+      const response = await cladClient.getProcessedInput(
+        "Im looking for an e-bike reccommendation to get around the city",	// This can be any text input
+        userId,
+        "true"
+      );
+      console.log("✅ Response:", response);
+      console.log("Prompt:", response.prompt);
+			setLatestResponse(response); // ✅ store response in state
+    };
+
+    run();
+  }, []);
 
   const handleSend = () => {
     if (!input.trim()) return;
@@ -17,6 +45,12 @@ export default function App() {
     if (e.key === 'Enter') handleSend();
   };
 
+	// 5. Make a new object to have a map to the response from #4
+	// this is because we need to check if the promptType is 'injected' to show the ad or not
+	const adMessage = {
+		prompted: latestResponse
+	}
+
   return (
     <div className="chat-container">
       <div className="messages">
@@ -24,6 +58,12 @@ export default function App() {
           <ChatMessage key={idx} sender={msg.sender} text={msg.text} />
         ))}
       </div>
+			{/* 6. Conditionally render the AdCard if the response is injected */}
+			{latestResponse?.promptType === 'injected' && (
+				<div className="mt-4">
+					<AdCard prompted={latestResponse} className="my-2" />
+				</div>
+			)}
       <div className="input-box">
         <input
           value={input}
